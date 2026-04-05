92ef85fa79f9465e18d999d8efb00e2db26c8e95
joselito-v <jlvillarta@gmail.com>
2026-04-05 20:16:55 +0800

    Update work-history.js

--- work-history.js
+++ work-history.js
@@ -50,7 +50,7 @@ export async function init(containerId) {
                 </div>
 
                 <div class="sort-container" style="margin-bottom: 0; margin-left: auto;">
-                    <button class="sort-btn" id="sortToggle"><i class="fas fa-arrow-down" id="sortIcon">Date</i>
+                    <button class="sort-btn" id="sortToggle"><i class="fas fa-arrow-down" id="sortIcon"> Date</i>
                     </button>
                 </div>
             </div>
