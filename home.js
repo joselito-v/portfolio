34bfade715a38b61caab31db97bf9da3b0dc7840
joselito-v <jlvillarta@gmail.com>
2026-04-06 08:37:37 +0800

    Update home.js

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ export function init(containerId) {
             <h2>Accounting Professional, College Instructor, and Web Developer</h2>
             <div>
                 <a href="#websites" class="btn btn-primary">My Websites</a>
-                <a href="#work-history" class="btn btn-primary">My Work</a>
+                <a href="#experience" class="btn btn-primary">My Work</a>
                 <a href="#contact" class="btn btn-outline">Get in Touch</a>
             </div>
         </section>
